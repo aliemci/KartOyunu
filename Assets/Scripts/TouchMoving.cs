0aa1f1be87c07b859d8bc0b13645cd84813aadfc
aliemci <aliemirci@gmail.com>
2020-01-26 14:46:38 +0300

    Deste & Girdi
    Desteleme eklendi.
    Hem fare hem dokunmatik desteği eklendi.

--- Assets/Scripts/TouchMoving.cs
+++ Assets/Scripts/TouchMoving.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class TouchMoving : MonoBehaviour
 {
-
+    Transform originalParent;
 
     private void Start()
     {
-
+        originalParent = transform.parent;
     }
 
     private void OnMouseDown()
     {
-
+        transform.SetParent(transform.parent.parent);
     }
 
     private void OnMouseDrag()
     {
-        transform.position = Vector3.Lerp(transform.position, Input.GetTouch(0).position, Time.deltaTime * 5);
+        if(Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            transform.position = Vector3.Lerp(transform.position, Input.GetTouch(0).position, Time.deltaTime * 5);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, Input.mousePosition, Time.deltaTime * 5);
+        }
     }
 
     private void OnMouseUp()
     {
-        
+        transform.SetParent(originalParent);
     }
 }
