069c9fed5b9f75baaaf6499272cedbd407e40614
aliemci <aliemirci@gmail.com>
2020-01-31 19:57:37 +0300

    Hareket sorunu çözüldü
    OnDrag fonksiyonunda olan setParent işlevi, BeginDrag fonksiyonu içine taşındı.

--- Assets/Scripts/TouchMoving.cs
+++ Assets/Scripts/TouchMoving.cs
@@ -42,30 +42,39 @@ public class TouchMoving : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        //Kartı tuttuğu yer bir değişkene atılıyor.
         grabOffset = Input.mousePosition - transform.position;
+        transform.SetParent(transform.parent.parent);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.SetParent(transform.parent.parent);
         if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
         {
-            transform.position = Vector3.Lerp(transform.position, Input.GetTouch(0).position + grabOffset, Time.deltaTime * 5);
+            //transform.position = Vector3.Lerp(transform.position, Input.GetTouch(0).position + grabOffset, Time.deltaTime * 5);
+            transform.position = Input.GetTouch(0).position + grabOffset;
         }
         else
         {
             Vector3 grabOffset3 = grabOffset;
-            transform.position = Vector3.Lerp(transform.position, Input.mousePosition + grabOffset3, Time.deltaTime * 5);
+            //transform.position = Vector3.Lerp(transform.position, Input.mousePosition + grabOffset3, Time.deltaTime * 5);
+            transform.position = Input.mousePosition + grabOffset3;
         }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        //Fare'nin konumuna bir ışın gönderiliyor.
         RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, -Vector2.up, 500f);
+
+        //Işın bir şeye çarptıysa:
         if (hit.collider != null)
         {
+            //Düşmanın üstüne isabet ettiyse "DamageTaken" işlevi çağırılıyor.
             hit.collider.gameObject.GetComponent<EnemyDisplay>().DamageTaken(GetComponent<CardDisplay>().card.Attack);
         }
+
+        //Kartı desteye koyma
         transform.SetParent(originalParent);
     }
 }
