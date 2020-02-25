997b7815f36165cfb075fcb0bcc3e9cc1cd7d16f
aliemci <aliemirci@gmail.com>
2020-02-26 00:06:52 +0300

    envanter eklendi
    Envanter tuşu eklendi. Kısa olarak kodu yazıldı. Envanter sahnesi oluşturuldu. Bu sahneye geçiş sağlandı.

---
+++ Assets/Resources/Scripts/Scene/fight_buttons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class fight_buttons : MonoBehaviour
+{
+    
+    public void inventory()
+    {
+        SceneManager.LoadScene("inventory");
+    }
+
+    public void card_pile()
+    {
+        SceneManager.LoadScene("card_pile");
+    }
+}
