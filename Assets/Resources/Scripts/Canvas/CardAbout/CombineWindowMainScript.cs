f270a558312df46fe4ba096f139f1210ee3878b0
aliemci <aliemirci@gmail.com>
2020-06-03 00:47:54 +0300

    Stun ve Chemical Grenade kart tipleri eklendi

--- Assets/Resources/Scripts/Canvas/CardAbout/CombineWindowMainScript.cs
+++ Assets/Resources/Scripts/Canvas/CardAbout/CombineWindowMainScript.cs
@@ -63,20 +63,69 @@ public class CombineWindowMainScript : MonoBehaviour
             if((comTypeCard1 == CombineType.Pressure && comTypeCard2 == CombineType.Flame) || (comTypeCard1 == CombineType.Flame && comTypeCard2 == CombineType.Pressure))
             {
                 Debug.Log("THERMITE!!!!");
+
+                //Her yere vuruyor
                 float[] cardAttackRange = { 0.25f, 0.25f, 0.25f, 0.25f };
 
-
-                //Card will_card = new Card("Thermite", CardType1.DebuffCard, CardType2.None, buffs.None, debuffs.Burn, CombineType.None, 0f, 5f, cardAttack, cardDefence, cardMana, true, false, cardAttackRange);
+                //Yeni bir kart oluşturuyor.
                 Card will_card = ScriptableObject.CreateInstance("Card") as Card;
 
+                //Kartın özelliklerini atıyor
                 will_card.cardName = "Thermite";
                 will_card.attack = cardAttack;
                 will_card.defence = cardDefence;
                 will_card.mana = cardMana;
                 will_card.attack_range = cardAttackRange;
+                will_card.cardDebuff = debuffs.Burn;
 
                 //CardGenerator.create_new_card(CardGO, "Thermite Grenade", thermiteGrenade, transform.Find("CardSlotResult"));
                 GameObject createdCard = CardGenerator.create_new_card(CardGO, "Thermite Grenade", will_card, transform.Find("CardSlotResult"));
+            }
+
+            //Basınç + Elektrik = Stun Grenade
+            if ((comTypeCard1 == CombineType.Pressure && comTypeCard2 == CombineType.Lightning) || (comTypeCard1 == CombineType.Lightning && comTypeCard2 == CombineType.Pressure))
+            {
+                Debug.Log("STUN!!!!");
+
+                //Her yere vuruyor
+                float[] cardAttackRange = { 0.25f, 0.25f, 0.25f, 0.25f };
+
+                //Yeni bir kart oluşturuyor.
+                Card will_card = ScriptableObject.CreateInstance("Card") as Card;
+
+                //Kartın özelliklerini atıyor
+                will_card.cardName = "Stun Grenade";
+                will_card.attack = 0;
+                will_card.defence = 0;
+                will_card.mana = cardMana;
+                will_card.attack_range = cardAttackRange;
+                will_card.cardDebuff = debuffs.Stun;
+
+                //CardGenerator.create_new_card(CardGO, "Thermite Grenade", thermiteGrenade, transform.Find("CardSlotResult"));
+                GameObject createdCard = CardGenerator.create_new_card(CardGO, "Stun Grenade", will_card, transform.Find("CardSlotResult"));
+            }
+
+            //Basınç + Zehir = Chemical Grenade
+            if ((comTypeCard1 == CombineType.Pressure && comTypeCard2 == CombineType.Toxic) || (comTypeCard1 == CombineType.Toxic && comTypeCard2 == CombineType.Pressure))
+            {
+                Debug.Log("CHEMICA!!!!");
+
+                //Her yere vuruyor
+                float[] cardAttackRange = { 0.25f, 0.25f, 0.25f, 0.25f };
+
+                //Yeni bir kart oluşturuyor.
+                Card will_card = ScriptableObject.CreateInstance("Card") as Card;
+
+                //Kartın özelliklerini atıyor
+                will_card.cardName = "Chemical Grenade";
+                will_card.attack = 0;
+                will_card.defence = 0;
+                will_card.mana = cardMana;
+                will_card.attack_range = cardAttackRange;
+                will_card.cardDebuff = debuffs.Weakness;
+
+                //CardGenerator.create_new_card(CardGO, "Thermite Grenade", thermiteGrenade, transform.Find("CardSlotResult"));
+                GameObject createdCard = CardGenerator.create_new_card(CardGO, "Stun Grenade", will_card, transform.Find("CardSlotResult"));
             }
 
         }
